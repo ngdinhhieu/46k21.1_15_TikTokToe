fd50388706e70ba07d52faa8abccd3ff8694cc15
ngdinhhieu <108282201+ngdinhhieu@users.noreply.github.com>
2022-07-15 16:53:19 +0700

    Update index.js

---
+++ TikTokToe/js/index.js
@@ -0,0 +1,35 @@
+const point = prompt('Nhập số điểm ');
+const pointInt = parseInt(point);
+console.log('pointInt', typeof pointInt, pointInt);
+
+var dem_X=0
+var dem_O=0
+
+let board = ['', '', '', '', '', '', '', '', ''];
+let isGameActive = true;
+
+const PLAYERX_WON = 'X THẮNG';
+const PLAYERO_WON = 'O THẮNG';
+
+
+const ĐKThắng = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+if PLAYERX_WON = ĐKThắng;
+    dem_X = dem_X + 1
+
+
+
+
+
+if PLAYERO_WON = ĐKThắng;
+    dem_O = dem_O + 1
